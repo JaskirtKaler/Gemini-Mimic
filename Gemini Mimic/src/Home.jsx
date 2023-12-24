9fbbe4638b34620989536cec83ce6051986d45aa
Devesh Krishan <deveshkrishan2003@gmail.com>
2023-12-24 15:22:11 -0800

    left column is fixed

--- Gemini Mimic/src/Home.jsx
+++ Gemini Mimic/src/Home.jsx
@@ -1,8 +1,8 @@
-import './App.css';
+import './Home.css';
 import logo from "./logo.png";
 
 import { MagnifyingGlassCircleIcon, UserGroupIcon } from '@heroicons/react/24/solid';
-import { BookOpenIcon, UserIcon } from '@heroicons/react/24/outline';
+import { BookOpenIcon, UserIcon, ArrowRightEndOnRectangleIcon } from '@heroicons/react/24/outline';
 
 function Home(){
     return (
@@ -10,8 +10,11 @@ function Home(){
           <div className='wrapper'>
     
             <section className='left-column'>
-              <GeminiMimicHomePageTitle/>
-              <GeminiHomePageOptions/>
+              <div >
+                <GeminiMimicHomePageTitle/>
+                <GeminiHomePageOptions/>
+              </div>
+              <GeminiMimicHomePageLogOut/>
             </section>
     
             <section className='right-column'>
@@ -32,28 +35,43 @@ function GeminiMimicHomePageTitle(){
     )
   }
   
-  function GeminiHomePageOptions(){
-    const menuOptionsImages = [
-      <MagnifyingGlassCircleIcon className='img-home-wrapper'/> , 
-      <BookOpenIcon className='img-home-wrapper'/>,
-      <UserGroupIcon className='img-home-wrapper'/>,
-      <UserIcon className='img-home-wrapper'/>
-    ];
-  
-    const menuOptions = ["Explore Gemini" , "History", "About", "Account"];
-  
-    const listItems = menuOptions.map((option, index) => 
-    <div key={index} className="menu-item">
-        {menuOptionsImages[index]}
-      <h2>{option}</h2>
+function GeminiHomePageOptions(){
+  const menuOptionsImages = [
+    <MagnifyingGlassCircleIcon className='img-home-wrapper'/> , 
+    <BookOpenIcon className='img-home-wrapper'/>,
+    <UserGroupIcon className='img-home-wrapper'/>,
+    <UserIcon className='img-home-wrapper'/>
+  ];
+
+  const menuOptions = ["Explore Gemini" , "History", "About", "Account"];
+
+  const listItems = menuOptions.map((option, index) => 
+  <div key={index} className="menu-item">
+      {menuOptionsImages[index]}
+    <h2 className='home-text-font'>{option}</h2>
+  </div>
+  );
+
+  return (
+    <>
+        {listItems}
+    </>
+  )
+}  
+
+function GeminiMimicHomePageLogOut(){
+  return (
+    <div>
+      <div className='home-line'></div>
+
+      <div className='home-log-out'>
+        <h3 className='home-text-font'>Log out</h3>
+        <ArrowRightEndOnRectangleIcon className='img-home-wrapper'/>
+      </div>
+
     </div>
-    );
-  
-    return (
-      <>
-          {listItems}
-      </>
-    )
-  }  
+  )
+}
+
 
 export default Home;
