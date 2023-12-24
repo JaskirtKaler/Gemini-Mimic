dd90fb5c804b74c16b50fa42477406b61d51ef86
Devesh Krishan <deveshkrishan2003@gmail.com>
2023-12-24 11:44:13 -0800

    gemini mimic title completed

--- Gemini Mimic/src/App.jsx
+++ Gemini Mimic/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import logo from "../public/logo.png"
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='wrapper'>
+        <GeminiMimicTitle/>
       </div>
     </>
+  )
+}
+
+function GeminiMimicTitle(){
+  return (
+      <div className="gemini_title">
+        <img src={logo} id='gemini_logo'/>
+        <h1>Gemini Mimic</h1>
+      </div>
   )
 }
 
