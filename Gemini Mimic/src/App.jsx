340082958946bd5f9e5c908e74f2a4c5a5049ff5
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2023-12-24 13:11:49 -0800

    edits

--- Gemini Mimic/src/App.jsx
+++ Gemini Mimic/src/App.jsx
@@ -2,14 +2,23 @@ import './App.css'
 import logo from "../public/logo.png"
 import './Login.jsx'
 import Login from './Login'
-
+import Register from './Register'
 function App() {
   const login = true;
-
+  const register = true;
   if (login){
-    return(
-      <Login/>
-    )
+
+    if(register){
+      return(
+        <Login/>
+        // click btn so then we move to Register
+      )
+      
+    }else{
+      return(
+        <Register />
+      )
+    }
   }
   
   else{
