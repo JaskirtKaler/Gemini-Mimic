3259b2a465d97fb9edc160981c7172b202bee837
Devesh Krishan <deveshkrishan2003@gmail.com>
2023-12-24 12:38:00 -0800

    logos imported

--- Gemini Mimic/src/App.jsx
+++ Gemini Mimic/src/App.jsx
@@ -1,5 +1,8 @@
-import './App.css'
-import logo from "../public/logo.png"
+import './App.css';
+import logo from "../public/logo.png";
+// import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassCircleIcon, UserGroupIcon } from '@heroicons/react/24/solid';
+import { BookOpenIcon, UserIcon } from '@heroicons/react/24/outline';
 
 function App() {
 
@@ -7,8 +10,9 @@ function App() {
     <>
       <div className='wrapper'>
 
-        <section>
-          <GeminiMimicTitle/>
+        <section className='left-column'>
+          <GeminiMimicHomePageTitle/>
+          <GeminiHomePageOptions/>
         </section>
 
         <section className='right-column'>
@@ -20,7 +24,7 @@ function App() {
   )
 }
 
-function GeminiMimicTitle(){
+function GeminiMimicHomePageTitle(){
   return (
       <div className="gemini_title">
         <img src={logo} id='gemini_logo'/>
@@ -28,5 +32,32 @@ function GeminiMimicTitle(){
       </div>
   )
 }
+
+function GeminiHomePageOptions(){
+  const menuOptionsImages = [];
+  const menuOptions = ["Explore Gemini, History, About, Account"];
+
+  const listItems = menuOptions.map((option) => 
+    <div>
+
+      <p>{option}</p>
+    </div>
+    
+    );
+
+  return (
+    <>
+      <MagnifyingGlassCircleIcon/>
+      <BookOpenIcon/>
+      <UserGroupIcon/>
+      <UserIcon/>
+
+      <div>
+        {listItems}
+      </div>
+    </>
+  )
+}
+
 
 export default App
