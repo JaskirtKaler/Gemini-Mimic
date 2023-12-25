170f14cd0f803d7c8fdbcc37d4b4fa120f38c65b
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2023-12-24 16:19:05 -0800

    Firebase
    integrate firebase

--- Gemini Mimic/src/Login.jsx
+++ Gemini Mimic/src/Login.jsx
@@ -1,25 +1,65 @@
 import React, { useState } from 'react'
 import './Login.css'
+import { app } from './firebaseConfig'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('')
+  const [data, setData] = useState({})
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
 
   const handleLogin = () => {
     // handle login here
+    signInWithEmailAndPassword(auth, data.email, data.password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
     console.log(email, " ", password)
   }
+
+  
+  const handleInput = (event) =>{
+    let newInput = { [event.target.name]: event.target.value}
+    setData({ ...data, ...newInput});
+  }
+
+
+
   return (
     <div className="login-main">
       <div className="login-contents">
        <h1>Login</h1>
-       <label>
+       {/* <label>
         Email:
         <input type="email" value={email} onChange={e =>setEmail(e.target.value)}></input>
-       </label>
-       <label>
-        Password:
-        <input type="password" value={password} onChange={e =>setPassword(e.target.value)}></input>
-       </label>
+       </label> */}
+       <input 
+        name='email'
+        placeholder='Email'
+        onChange={(event) => handleInput(event)}
+       />
+       <input 
+        name='password'
+        placeholder='Password'
+        onChange={(event) => handleInput}
+        />
        <button onClick={handleLogin}>Login</button>
       </div>
     </div>
---
+++ Gemini Mimic/src/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+require('dotenv').config()
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
