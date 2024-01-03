b13f90dcda7e1585ea1753a422944eb4a5ea138d
Devesh Krishan <deveshkrishan2003@gmail.com>
2024-01-03 06:15:53 -0800

    fixed layout and linked react routes

--- Gemini Mimic/src/Logout.jsx
+++ Gemini Mimic/src/Logout.jsx
@@ -1,19 +1,27 @@
 import logo from "./logo.png";
 import './Logout.css';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function Logout(){
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/login'); 
+    }
+
     return (
         <div className="logo-wrapper">
-            <div className="logo-logout">
-                <img src={logo} id='gemini_logout_logo'/>
+            <div className="line-logo-wrapper">
+                <div className="logo-logout">
+                    <img src={logo} id='gemini_logout_logo'/>
+                </div>
+                <div className="logout-line"/>
             </div>
-            <div className="logout-line"/>
-
             <div className="main-ui-logout">
                 <h1 id="logged-out-text">Logged Out</h1>
                 <p id="thank-logout">Thank you for using Gemini Mimic!</p>
-                <Button variant="contained" size="large" style={{ backgroundColor: '#E75A7C', color: '#FFFFFF' }}
+                <Button variant="contained" size="large" onClick={handleLogin} style={{ backgroundColor: '#E75A7C', color: '#FFFFFF' }}
 >Sign In Again</Button>
             </div>
 
