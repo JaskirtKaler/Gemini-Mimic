828b2161aa915d417dc429bd936cfdedc6794645
Devesh Krishan <deveshkrishan2003@gmail.com>
2024-01-03 04:51:37 -0800

    fixed logout but need to fix layout

--- Gemini Mimic/src/Logout.jsx
+++ Gemini Mimic/src/Logout.jsx
@@ -1,11 +1,27 @@
 import logo from "./logo.png";
 import './Logout.css';
+import { Button } from '@mui/material';
 
 function Logout(){
     return (
-        <>
-            <img src={logo} id='gemini_logo'/>
-        </>
+        <div className="logo-wrapper">
+            <div className="logo-logout">
+                <img src={logo} id='gemini_logout_logo'/>
+            </div>
+            <div className="logout-line"/>
+
+            <div className="main-ui-logout">
+                <h1 id="logged-out-text">Logged Out</h1>
+                <p id="thank-logout">Thank you for using Gemini Mimic!</p>
+                <Button variant="contained" size="large" style={{ backgroundColor: '#E75A7C', color: '#FFFFFF' }}
+>Sign In Again</Button>
+            </div>
+
+            <div className="logout-footer">
+                <p id="footer-text"> © 2023 Jaskirt Kaler, Devesh Krishan. All rights reserved.</p>
+            </div>
+
+        </div>
     );
 }
 
